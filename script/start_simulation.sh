b28ab40cf9b1a4dc8b6f9cbbac2bf196fb20b933
Versicles <2712089295@qq.com>
2023-07-28 22:39:28 +0800

    [feat][fix] : fix same bug

--- script/start_simulation.sh
+++ script/start_simulation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 gnome-terminal -t "roscore" -x bash -c "roscore;exec bash;"
-sleep 5s
+sleep 1s
 
 gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;roslaunch uav_simulation env_simulation.launch;exec bash;"
 sleep 5s
 
 gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;roslaunch uav_simulation uav_simulation.launch;exec bash;"
-sleep 3s
+sleep 5s
 
 gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;roslaunch uav_simulation referee_system.launch;exec bash;"
 sleep 2s
@@ -15,9 +15,14 @@ sleep 2s
 gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;rosrun uav_simulation command_process.py;exec bash;"
 sleep 2s
 
-#gnome-terminal -t "apriltag_ros"   -x bash -c "source devel/setup.bash;roslaunch apriltag_ros  continuous_detection.launch;exec bash;"
-#sleep 2s
+gnome-terminal -t "apriltag_ros"   -x bash -c "source devel/setup.bash;roslaunch apriltag_ros  continuous_detection.launch;exec bash;"
+sleep 3s
 
+gnome-terminal -t "exploration_manager"   -x bash -c "source devel/setup.bash ;roslaunch exploration_manager auto_exploration.launch;exec bash;"
+sleep 3s
+
+gnome-terminal -t "exploration_manager"   -x bash -c "source devel/setup.bash ;roslaunch exploration_manager rviz.launch;exec bash;"
+sleep 3s
 #gnome-terminal -t "uav_simulation" -x bash -c "source devel/setup.bash;rosrun uav_simulation keyboard_control.py;exec bash;"
 #sleep 2s
 
---
+++ script/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gnome-terminal -x bash -c "sh ./script/start_simulation.sh"
